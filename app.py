5c7544d93e3a800b9e428ab8a56666f1826b2bdb
Raj Parekh <rajparekh144@gmail.com>
2021-02-07 11:37:10 +0530

    deployment changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for,request
 from utils import retrieve_result
 from query_filter import get_result
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,4 +20,5 @@ def predict():
         return render_template("prediction.html",data = res=="positive",sentence=sentence,dis="Your Subject = ")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
